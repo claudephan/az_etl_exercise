76a074ac62c64f92a2616a00b3cf04459d08a946
x540098 <claudephan@gmail.com>
2021-10-19 23:54:57 -0700

    created vm provision python script

---
+++ provision_vm.py
@@ -0,0 +1,14 @@
+# Import the needed credential and management objects from the libraries.
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+import os
+
+print(f"Provisioning a virtual machine...some operations might take a minute or two.")
+
+# Acquire a credential object using CLI-based authentication.
+credential = AzureCliCredential()
+
+# Retrieve subscription ID from environment variable.
+subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
